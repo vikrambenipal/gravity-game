36dbf9fe1491c7c1bfe610496bab53c7dc202cf5
vikrambenipal <vikrambenipal@gmail.com>
2020-11-08 12:28:36 -0500

    lasers work in progress

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float maxDistance; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, maxDistance);
+        Debug.DrawLine(transform.position, transform.position + transform.up * maxDistance, Color.green);
+
+
+        if (hitInfo)
+        {
+            if (hitInfo.collider.tag == "Player")
+            {
+                Debug.Log("3");
+                Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.up * maxDistance, Color.green);
+        }
+
+        
+
+        /*
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.up * maxDistance, Color.green);
+        }
+        */
+    }
+}
