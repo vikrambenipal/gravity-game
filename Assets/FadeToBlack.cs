97978f30eee5330c8b743a717bf23c3106321c8e
vikrambenipal <vikrambenipal@gmail.com>
2020-11-17 01:03:41 -0500

    scene transitions

---
+++ Assets/FadeToBlack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeToBlack : MonoBehaviour
+{
+    public static FadeToBlack instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void playerDied()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-
+    public Animator transition; 
     public static LevelManager instance;
     public float respawnTime;
+    public float deathAnimationTime;
 
     // Start is called before the first frame update
     void Start()
@@ -28,15 +29,21 @@ public class LevelManager : MonoBehaviour
     private IEnumerator RespawnCo()
     {
         PlayerController.instance.deathParticleEffect.SetActive(true);
+
         Instantiate(PlayerController.instance.deathParticleEffect, PlayerController.instance.transform.position, Quaternion.identity);
+        PlayerController.instance.gameObject.SetActive(false);
+        yield return new WaitForSeconds(deathAnimationTime);
         PlayerController.instance.deathParticleEffect.SetActive(false);
 
-        PlayerController.instance.gameObject.SetActive(false);
+
+        transition.SetTrigger("Start");
         yield return new WaitForSeconds(respawnTime);
 
         PlayerController.instance.transform.position = CheckpointController.instance.spawnPoint;
         PlayerController.instance.gameObject.SetActive(true);
         PlayerController.instance.ResetPlayer();
+        transition.SetTrigger("End");
     }
+
 
 }
