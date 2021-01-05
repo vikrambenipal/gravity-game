5f3b3968e057ba4d74c3ddc5113474cb7eceae20
vikrambenipal <vikrambenipal@gmail.com>
2021-01-05 14:52:46 -0500

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // used to move between scenes 
+
+public class MainMenu : MonoBehaviour
+{
+    public string startScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void QuitGame()
+    {
+        // only works when you build the game
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+}
